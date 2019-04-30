d8e5fc48fa8a390be0022341f69238254367c8b8
Tom Woodward <twwoodward@me.com>
2019-04-30 14:24:47 -0400

    minor css

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -32,7 +32,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
 		wp_enqueue_script( 'jquery');
 		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false, true);
-
+		
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/' . asset_path('js/theme.min.js'), array(), null, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
