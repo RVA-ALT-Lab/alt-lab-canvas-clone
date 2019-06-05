8e84037c7e5dec82a44d6ee41ab881d81cea3fb9
Tom Woodward <twwoodward@me.com>
2019-06-05 10:04:37 -0400

    simplify by destroying things

--- page.php
+++ page.php
@@ -11,43 +11,49 @@
  */
 
 get_header();
-
-$container   = get_theme_mod( 'understrap_container_type' );
-
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="page-wrapper">
+<div class="wrapper <?php echo get_theme_mod( 'body_background_choice' );?>" id="page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+			<?php get_sidebar( 'left' ); ?>
 
-			<main class="site-main" id="main">
+			<div
+				class="col-md-8 content-area"
+				id="primary">
+				<?php 
+					if ( current_user_can('editor') || current_user_can('administrator') ){
+						edit_post_link( 'Edit', '<div class="edit-header"><div class="edit-btn">', '<i class="fa fa-pencil" aria-hidden="true"></i></div></div>' );
+					}
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				?>
 
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+				<main class="site-main" id="main" role="main">
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php endwhile; // end of the loop. ?>
+						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-			</main><!-- #main -->
+						<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+						?>
 
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-		 
-	</div><!-- .row -->
+					<?php endwhile; // end of the loop. ?>
 
-</div><!-- Container end -->
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row -->
+
+	</div><!-- Container end -->
 
 </div><!-- Wrapper end -->
 
